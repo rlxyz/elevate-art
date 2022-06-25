de22dc68527e564c81938ccac55b69b2345359ca
Martin Valentino <martin.david.siagian@gmail.com>
2022-06-25 22:27:50 +1000

    Add onClick props to the Button component

--- components/Minter/MintButton.tsx
+++ components/Minter/MintButton.tsx
@@ -4,14 +4,16 @@ import React from 'react'
 interface MintButtonProps {
   disabled?: boolean
   mintCount: number
+  onClick: () => void
 }
 
 export const MintButton: React.FC<MintButtonProps> = ({
   mintCount,
+  onClick,
   disabled = false,
 }) => {
   return (
-    <Button fullWidth disabled={disabled}>
+    <Button fullWidth disabled={disabled} onClick={onClick}>
       <span className="font-bold">{`Mint ${mintCount}`}</span>{' '}
       <span className="font-light">NFTs</span>
     </Button>
