1b29687a58a131eca4ab19001d53793c2acba1a0
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-08-16 21:51:51 +1000

    added rest of the files

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      hue: {
+        light: '#FDFDF9', // example usage
+        dark: '#0F111A',
+      },
+      white: colors.white,
+      primary: colors.red,
+      lightGray: '#E7E7E7',
+      darkGrey: '#959595',
+      redDot: '#FF5555',
+      greenDot: '#86D893',
+      black: '#2C2C2C',
+      disabledGray: '#D7D7D7',
+      blueHighlight: '#6484F3',
+      redError: '#FF5555',
+    },
+    fontFamily: {
+      sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      normal: ['"Plus Jakarta Sans"', 'sans-serif'],
+      'gilroy-extra-bold': ['"Gilroy-ExtraBold"', 'sans-serif'],
+      'plus-jakarta-sans': ['"Plus Jakarta Sans"', 'sans-serif'],
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+      '3xl': '1536px',
+      '4xl': '2048px',
+      '5xl': '2560px',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
