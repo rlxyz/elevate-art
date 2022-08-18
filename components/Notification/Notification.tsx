9579d09b9b3f94973d0a6d6a8d7ad14d62d115b2
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-08-18 12:30:20 +1000

    done

--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -10,7 +10,7 @@ export const Notification = ({ children, id }: Props) => {
   return (
     <div
       id={id}
-      className='relative p-8 w-[350px] max-w-lg rounded-lg border border-lightGray shadow z-5 bg-white'
+      className='relative p-6 w-[350px] max-w-lg rounded-lg border border-lightGray shadow z-5 bg-white'
       role='alert'
     >
       <div className='w-full flex items-center'>{children}</div>
