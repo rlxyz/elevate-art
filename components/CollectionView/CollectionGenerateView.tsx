4193b03171016465d0e4ee60d6e037556285bd76
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-08-20 20:15:14 +1000

    now infinite scroll works but there is a scrolling issue

--- components/CollectionView/CollectionGenerateView.tsx
+++ components/CollectionView/CollectionGenerateView.tsx
@@ -9,55 +9,123 @@ import { ethers } from 'ethers'
 import Image from 'next/image'
 import { NextRouter, useRouter } from 'next/router'
 import React, { ReactNode, useCallback, useEffect, useState } from 'react'
-import InfiniteScroll from 'react-infinite-scroller'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 import { CollectionViewContent } from './ViewContent'
 
-const createCollectionSeed = (collectionId: string, generation: number) => {
-  return parseInt(
-    ethers.utils
-      .keccak256(ethers.utils.toUtf8Bytes(`${collectionId}-${generation}`))
-      .toString(),
-    16
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+}
+
+const CollectionInfiniteScrollItem = ({
+  token,
+  repositoryName,
+  organisationName,
+  name,
+}: {
+  token: any
+  repositoryName: string
+  organisationName: string
+  name: string
+}) => {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('show')
+    }
+  }, [controls, inView])
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+      transition: { ease: [0.78, 0.14, 0.15, 0.86] },
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { ease: [0.78, 0.14, 0.15, 0.86] },
+    },
+  }
+  return (
+    <motion.div
+      className='flex flex-col'
+      variants={item}
+      initial='hidden'
+      animate={controls}
+      ref={ref}
+    >
+      <div
+        className='h-[120px] overflow-hidden'
+        style={{ transformStyle: 'preserve-3d' }}
+      >
+        {token.attributes.map((attribute, index) => {
+          return (
+            <div
+              className='absolute w-full h-full flex flex-col items-center'
+              key={index}
+            >
+              <img
+                className='rounded-[5px] border-[1px] border-lightGray'
+                src={`${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/image/upload/${
+                  process.env.NEXT_PUBLIC_CLOUDINARY_LOW_RES_IMAGES
+                    ? 'c_fill,h_200,w_200'
+                    : ''
+                }/v1/${organisationName}/${repositoryName}/layers/${toPascalCaseWithSpace(
+                  attribute['trait_type']
+                )}/${toPascalCaseWithSpace(attribute['value'])}.png`}
+              />
+            </div>
+          )
+        })}
+      </div>
+      <span className='text-xs'>{name}</span>
+    </motion.div>
   )
 }
 
-const COLLECTION_GENERATION_SIZE = Number(
-  process.env.NEXT_PUBLIC_COLLECTION_GENERATION_SIZE || 50
-)
-
-const COLLECTION_GENERATION_MAX_SIZE_PER_PAGE = Number(
-  process.env.NEXT_PUBLIC_COLLECTION_GENERATION_MAX_SIZE_PER_PAGE || 200
-)
-
 const CollectionInfiniteScroll = () => {
   const [tokens, setTokens] = useState([])
-  const [nextStartPoint, setNextStartPoint] = useState(0)
-  const [fetching, setFetching] = useState(true)
-
+  const [page, setPage] = useState(1)
   const router: NextRouter = useRouter()
   const organisationName: string = router.query.organisation as string
   const repositoryName: string = router.query.repository as string
-  const { collection, repository, artCollection, setArtCollection } =
-    useCompilerViewStore((state) => {
-      return {
-        collection: state.collection,
-        regenerate: state.regenerate,
-        repository: state.repository,
-        artCollection: state.artCollection,
-        setArtCollection: state.setArtCollection,
-      }
-    })
+  const { collection, repository } = useCompilerViewStore((state) => {
+    return {
+      collection: state.collection,
+      regenerate: state.regenerate,
+      repository: state.repository,
+    }
+  })
+
+  const createCollectionSeed = (collectionId: string, generation: number) => {
+    return parseInt(
+      ethers.utils
+        .keccak256(ethers.utils.toUtf8Bytes(`${collectionId}-${generation}`))
+        .toString(),
+      16
+    )
+  }
+
+  const COLLECTION_GENERATION_SIZE = Number(
+    process.env.NEXT_PUBLIC_COLLECTION_GENERATION_SIZE || 50
+  )
 
   const app: App = createCompilerApp(repositoryName)
 
-  const fetchItems = useCallback(() => {
-    if (!fetching) {
-      return
-    }
-
-    const startPointIndex = nextStartPoint
-    const endPointIndex = nextStartPoint + 1
+  const fetch = (start: number) => {
+    const startPointIndex = start
+    const endPointIndex = start + 1
     const startPoint = startPointIndex * COLLECTION_GENERATION_SIZE
     const endPoint = endPointIndex * COLLECTION_GENERATION_SIZE
 
@@ -66,55 +134,51 @@ const CollectionInfiniteScroll = () => {
       startPoint,
       endPoint
     )
-
     setTokens([...tokens, ...newTokens])
-    setNextStartPoint(endPointIndex)
+    setPage((p) => p + 1)
+  }
 
-    if (endPoint > COLLECTION_GENERATION_MAX_SIZE_PER_PAGE - 1) {
-      setFetching(false)
+  const fetchMoreData = (page: number) => {
+    if (page > collection.totalSupply - 1) {
+      return
     }
-  }, [tokens, fetching, nextStartPoint, collection])
+
+    return fetch(page)
+  }
 
   useEffect(() => {
-    fetchItems()
+    fetch(page)
   }, [])
 
+  if (!tokens.length) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className='grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 xl:grid-cols-7'>
-      {tokens.map((token, index) => {
-        return (
-          <div className='flex flex-col' key={index}>
-            <div
-              className='h-[120px] overflow-hidden'
-              style={{ transformStyle: 'preserve-3d' }}
-            >
-              {token.attributes.map((attribute, index) => {
-                return (
-                  <div
-                    className='absolute w-full h-full flex flex-col items-center'
-                    key={index}
-                  >
-                    <img
-                      className='rounded-[5px] border-[1px] border-lightGray'
-                      src={`${
-                        process.env.NEXT_PUBLIC_CLOUDINARY_URL
-                      }/image/upload/${
-                        process.env.NEXT_PUBLIC_CLOUDINARY_LOW_RES_IMAGES
-                          ? 'c_fill,h_200,w_200'
-                          : ''
-                      }/v1/${organisationName}/${repositoryName}/layers/${toPascalCaseWithSpace(
-                        attribute['trait_type']
-                      )}/${toPascalCaseWithSpace(attribute['value'])}.png`}
-                    />
-                  </div>
-                )
-              })}
-            </div>
-            <span className='text-xs'>{`${repository.tokenName} #${index}`}</span>
-          </div>
-        )
-      })}
-    </div>
+    <InfiniteScroll
+      dataLength={tokens.length}
+      next={() => fetchMoreData(page)}
+      hasMore={true}
+      loader={<div className='w-full h-full flex items-center'>...</div>}
+    >
+      <motion.div
+        className='grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 xl:grid-cols-7'
+        initial='hidden'
+        animate='show'
+        variants={container}
+      >
+        {tokens.map((token, index) => {
+          return (
+            <CollectionInfiniteScrollItem
+              token={token}
+              repositoryName={repositoryName}
+              organisationName={organisationName}
+              name={`${repository.tokenName} #${index}`}
+            />
+          )
+        })}
+      </motion.div>
+    </InfiniteScroll>
   )
 }
 
