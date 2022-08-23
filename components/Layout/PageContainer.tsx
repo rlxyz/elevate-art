0c9ae6d0bd1b3e846ca9e3b5d48f58f108adcc75
Martin David Valentino <martin.valentino@cultureamp.com>
2022-08-23 20:02:13 +1000

    Refine some styling

--- components/Layout/PageContainer.tsx
+++ components/Layout/PageContainer.tsx
@@ -14,7 +14,7 @@ export const PageContainer: React.FC<PageContainerProps> = ({
   return (
     <>
       {header}
-      <div className="px-5 lg:px-16 2xl:px-32 py-12 pb-20 grid gap-4 grid-cols-2">
+      <div className="px-5 lg:px-16 2xl:px-32 py-12 pb-20 grid gap-4 grid-cols-1 md:grid-cols-2">
         <div>{leftContent}</div>
         <div className="ml-5">{rightContent}</div>
       </div>
