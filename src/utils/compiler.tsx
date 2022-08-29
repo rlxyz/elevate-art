fcb2dced448f0de2e50c5016adfb450968c6af5c
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-08-29 13:24:14 +1000

    added create single token function

--- src/utils/compiler.tsx
+++ src/utils/compiler.tsx
@@ -1,6 +1,40 @@
 import seedrandom from 'seedrandom'
 import { LayerElement, TraitElement, Rules } from '@prisma/client'
 
+export const createToken = (opts: {
+  id: number
+  name: string
+  generation: number
+  layers: (LayerElement & {
+    traitElements: (TraitElement & {
+      rulesPrimary: (Rules & {
+        primaryTraitElement: TraitElement
+        secondaryTraitElement: TraitElement
+      })[]
+      rulesSecondary: (Rules & {
+        primaryTraitElement: TraitElement
+        secondaryTraitElement: TraitElement
+      })[]
+    })[]
+  })[]
+}) => {
+  const { id, name, generation, layers } = opts
+  const random = seedrandom(`${name}.${generation}.${id}`)
+  let elements: TraitElement[] = []
+  layers.forEach(({ traitElements }) => {
+    let r = Math.floor(random() * traitElements.reduce((a, b) => a + b.weight, 0))
+    traitElements.every((traitElement) => {
+      r -= traitElement.weight
+      if (r < 0) {
+        elements.push(traitElement)
+        return false
+      }
+      return true
+    })
+  })
+  return elements
+}
+
 export const createManyTokens = (
   layers: (LayerElement & {
     traitElements: (TraitElement & {
@@ -24,24 +58,9 @@ export const createManyTokens = (
     .forEach(({ traitElements }: LayerElement & { traitElements: TraitElement[] }) =>
       traitElements.sort((a, b) => a.weight - b.weight)
     )
-  const allElements: TraitElement[][] = []
-  for (let i = 0, random = seedrandom(`${name}.${generation}`); i < totalSupply; i++) {
-    let elements: TraitElement[] = []
-    layers.forEach(({ traitElements }) => {
-      let r = Math.floor(random() * traitElements.reduce((a, b) => a + b.weight, 0))
-      traitElements.every((traitElement) => {
-        r -= traitElement.weight
-        if (r < 0) {
-          elements.push(traitElement)
-          return false
-        }
-        return true
-      })
-    })
-    allElements.push(elements)
-  }
-
-  return allElements
+  return Array.from(Array(totalSupply).keys()).map((id: number) => {
+    return createToken({ id, name, generation, layers })
+  })
 }
 
 export const getTraitMappings = (allElements: TraitElement[][]) => {
