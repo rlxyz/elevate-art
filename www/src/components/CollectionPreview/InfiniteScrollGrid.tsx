83ff3c9d1728b442b8a6f5904704721366a99e3a
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-08-31 22:18:16 +1000

    fixed issue with Preview collection naming convention

--- www/src/components/CollectionPreview/InfiniteScrollGrid.tsx
+++ www/src/components/CollectionPreview/InfiniteScrollGrid.tsx
@@ -66,7 +66,7 @@ const InfiniteScrollGridItems = ({ tokensOnDisplay }: { tokensOnDisplay: number[
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 xl:gap-y-6 xl:gap-x-6 overflow-hidden'>
-      {populateCollection().map((item) => item)}
+      {repository && populateCollection().map((item) => item)}
     </div>
   )
 }
