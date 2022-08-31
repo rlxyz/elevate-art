5c7bd19c579d10496f7de8cb5f6063af60d357f7
Rosh Ghadamian <rghadamian@Roshs-MacBook-Pro.local>
2022-08-31 14:16:45 +1000

    adding zora docs template

---
+++ docs/src/components/content/ProjectsCardsContent.js
@@ -0,0 +1,63 @@
+import PartyBid from '@site/static/img/projects/partyBid.png'
+import PunkHouse from '@site/static/img/projects/punkHouse.png'
+import Mirror from '@site/static/img/projects/mirror.png'
+import BlitHouse from '@site/static/img/projects/blitHouse.png'
+import Fwb from '@site/static/img/projects/fwb.png'
+import Catalog from '@site/static/img/projects/catalog.png'
+import BrightMoments from '@site/static/img/projects/brightMoments.png'
+import Artiva from '@site/static/img/projects/artiva.png'
+import Glass from '@site/static/img/projects/glass.png'
+import OtisHouse from '@site/static/img/projects/otisHouse.png'
+import Poolsuite from '@site/static/img/projects/poolsuite.png'
+import Well from '@site/static/img/projects/theWell.png'
+
+export const projectsContent = [
+  {
+    project: PartyBid,
+    link: 'https://partybid.app',
+  },
+  {
+    project: Fwb,
+    link: 'https://gallery.fwb.help/',
+  },
+  {
+    project: Mirror,
+    link: 'https://mirror.xyz/',
+  },
+  {
+    project: Catalog,
+    link: 'https://catalog.works/',
+  },
+  {
+    project: BlitHouse,
+    link: 'https://blit.house/',
+  },
+  {
+    project: Artiva,
+    link: 'https://artiva.app',
+  },
+  {
+    project: PunkHouse,
+    link: 'https://punks.house/',
+  },
+  {
+    project: BrightMoments,
+    link: 'https://live.brightmoments.io/',
+  },
+  {
+    project: Glass,
+    link: 'https://glass.xyz/',
+  },
+  {
+    project: OtisHouse,
+    link: 'https://www.otishouse.xyz/',
+  },
+  {
+    project: Poolsuite,
+    link: 'https://poolsuite.net/',
+  },
+  {
+    project: Well,
+    link: 'https://teaser.the-well.is/',
+  },
+]
