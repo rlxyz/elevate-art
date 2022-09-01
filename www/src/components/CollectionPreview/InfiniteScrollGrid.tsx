66049a2d55ecc267342a1dc813256d2d6e996be4
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-09-01 10:51:05 +1000

    done

--- www/src/components/CollectionPreview/InfiniteScrollGrid.tsx
+++ www/src/components/CollectionPreview/InfiniteScrollGrid.tsx
@@ -65,8 +65,8 @@ const InfiniteScrollGridItems = ({ tokensOnDisplay }: { tokensOnDisplay: number[
   }
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 xl:gap-y-6 xl:gap-x-6 overflow-hidden'>
-      {repository && populateCollection().map((item) => item)}
+    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 4xl:grid-cols-6 xl:gap-y-6 xl:gap-x-6 overflow-hidden'>
+      {repository && layers && populateCollection().map((item) => item)}
     </div>
   )
 }
