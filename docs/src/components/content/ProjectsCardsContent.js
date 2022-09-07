cd187572ec90f4c9b0b55ad93cacffe7dec3b8b9
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2022-09-07 12:39:00 +1000

    updated logos for partners with ngv + proof

--- docs/src/components/content/ProjectsCardsContent.js
+++ docs/src/components/content/ProjectsCardsContent.js
@@ -1,15 +1,10 @@
-import Proof from '@site/static/img/projects/proof.jpeg'
-import Zora from '@site/static/img/zoraOrb.svg'
 import DreamLab from '@site/static/img/projects/dreamlab.jpeg'
-import Rektguy from '@site/static/img/projects/rekt.jpeg'
 import Fellowship from '@site/static/img/projects/fellowship.jpeg'
 import FortuneFriends from '@site/static/img/projects/ffc.gif'
-import BrightMoments from '@site/static/img/projects/brightMoments.png'
+import NGV from '@site/static/img/projects/ngv_logo_variations_2.gif'
+import Proof from '@site/static/img/projects/proof.jpeg'
+import Rektguy from '@site/static/img/projects/rekt.jpeg'
 import Roboghosts from '@site/static/img/projects/roboghosts.jpeg'
-import Glass from '@site/static/img/projects/glass.png'
-import OtisHouse from '@site/static/img/projects/otisHouse.png'
-import Poolsuite from '@site/static/img/projects/poolsuite.png'
-import Well from '@site/static/img/projects/theWell.png'
 
 export const projectsContent = [
   {
@@ -35,11 +30,11 @@ export const projectsContent = [
   {
     project: Roboghosts,
     link: 'https://www.roboghosts.com/',
-   },
-  // {
-  //   project: Zora,
-  //   link: 'https://zora.co/',
-  // },
+  },
+  {
+    project: NGV,
+    link: 'https://ngv.vic.gov.au/',
+  },
   // {
   //   project: BrightMoments,
   //   link: 'https://live.brightmoments.io/',
