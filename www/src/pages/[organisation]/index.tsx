8f62f7cfa9a8cef5870a194d6184f9c6593257df
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-09-19 11:52:20 +1000

    added auth to core organisation route

--- www/src/pages/[organisation]/index.tsx
+++ www/src/pages/[organisation]/index.tsx
@@ -1,9 +1,12 @@
 import { Layout } from '@components/Layout/Layout'
 import useOrganisationNavigationStore from '@hooks/useOrganisationNavigationStore'
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { getToken } from 'next-auth/jwt'
+import { getSession } from 'next-auth/react'
 import dynamic from 'next/dynamic'
 import { NextRouter, useRouter } from 'next/router'
 import { OrganisationNavigationEnum } from 'src/types/enums'
+import { prisma } from '../../server/db/client'
 
 const DynamicViewOrganisation = dynamic(() => import('@components/Views/ViewOrganisation'), { suspense: true })
 
@@ -30,6 +33,19 @@ const Page: NextPage = () => {
       </Layout>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { organisation } = context.query
+  const session = await getSession(context)
+  const token = await getToken({ req: context.req })
+  const userId = token?.sub ?? null
+  if (!userId) return { redirect: { destination: '/404', permanent: false } }
+  const data = await prisma.organisation.findFirst({
+    where: { name: organisation as string, admins: { some: { userId: userId } } },
+  })
+  if (!data) return { redirect: { destination: `/404`, permanent: false } }
+  return { props: { userId, session } }
 }
 
 export default Page
