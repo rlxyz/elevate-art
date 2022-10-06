588d6d03812bb573ceacbaa0c987946cbcda34af
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-06 16:45:34 +1100

    added notifications for success

--- www/src/hooks/mutations/useMutateOrganisationAddUser.tsx
+++ www/src/hooks/mutations/useMutateOrganisationAddUser.tsx
@@ -1,6 +1,12 @@
+import { useNotification } from '@hooks/utils/useNotification'
 import { trpc } from '@utils/trpc'
 
-// @todo implement this
+// @todo implement on success update
 export const useMutateOrganisationAddUser = () => {
-  return trpc.useMutation('organisation.addUser')
+  const { notifySuccess } = useNotification()
+  return trpc.useMutation('organisation.addUser', {
+    onSuccess: (data) => {
+      notifySuccess('New user added to organisation. It may take a few minutes for the changes to take reflect on the app.')
+    },
+  })
 }
