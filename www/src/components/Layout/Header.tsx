366501d1518ea86bfa32ea40808f98fcca61d106
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-07 11:53:37 +1100

    updates social routes

--- www/src/components/Layout/Header.tsx
+++ www/src/components/Layout/Header.tsx
@@ -24,7 +24,7 @@ const externalRoutes = [
 const socialRoutes = [
   {
     name: 'Twitter',
-    href: 'https://twitter.elevate.art',
+    href: 'https://twitter.com/elevate_art',
     icon: (props: any) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path d='M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84' />
@@ -33,7 +33,7 @@ const socialRoutes = [
   },
   {
     name: 'GitHub',
-    href: 'https://github.elevate.art',
+    href: 'https://github.com/rlxyz',
     icon: (props: any) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path
@@ -46,7 +46,7 @@ const socialRoutes = [
   },
   {
     name: 'Discord',
-    href: 'https://discord.elevate.art',
+    href: 'https://discord.gg/aC7spK59',
     icon: (props: any) => (
       <svg fill='currentColor' width='16' height='16' viewBox='0 0 71 55' {...props}>
         <path
@@ -62,7 +62,7 @@ const socialRoutes = [
 const HeaderExternalRoutes = () => {
   return (
     <div className='flex flex-row justify-center items-center space-x-3'>
-      <aside className='flex flex-row items-center justify-center space-x-3'>
+      {/* <aside className='flex flex-row items-center justify-center space-x-3'>
         {externalRoutes.map((item, index) => {
           return (
             <Link external={true} key={index} href={item.href}>
@@ -70,7 +70,7 @@ const HeaderExternalRoutes = () => {
             </Link>
           )
         })}
-      </aside>
+      </aside> */}
       {socialRoutes.map((item, index) => (
         <div key={index} className='cursor-pointer'>
           <Link external={true} href={item.href}>
