0d9bcdbb2f0cf63f52fbc7dc45e6407483368a8c
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2022-10-11 07:58:13 +1100

    changed logo details in footer

--- docs/src/theme/Footer/index.js
+++ docs/src/theme/Footer/index.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from 'react'
-import ElevateLogo from '../../../static/img/Logo_White.svg'
+import ElevateLogo from '../../../static/img/logo_white.svg'
 import QuestionsIcon from '../../../static/img/questionsIcon.svg'
 import TwitterIcon from '../../../static/img/twitterIcon.svg'
 import styles from './styles.module.css'
