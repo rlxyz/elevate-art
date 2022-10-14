3f85392de545ce3000855e4a9222d834f05ee7c7
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-14 15:07:59 +1100

    created apps

--- apps/www/src/pages/api/trpc/[trpc].ts
+++ apps/www/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from '@trpc/server/adapters/next'
-import { env } from '../../../env/server.mjs'
+import { env } from '../../../env/server.mjs.js.js'
 import { appRouter } from '../../../server/router'
 import { createContext } from '../../../server/router/context'
 
