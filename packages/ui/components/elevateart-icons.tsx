56696e202c855a9d9fd7dfb979a4195c8be5a294
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-19 23:04:09 +0800

    now fully exporting all components in ui

--- packages/ui/components/elevateart-icons.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * This exports all the icons that the application uses.
- *
- * @todo We don't use all these icons. Next update should remove the unused ones.
- */
-export * from "@heroicons/react/outline";
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,3 +2,7 @@ export * from "./components/breadcrumbs";
 export * from "./components/icons";
 export * from "./components/layout";
 export * from "./components/link";
+export * from "./components/loading";
+export * from "./components/modal";
+export * from "./components/tabs";
+export * from "./components/toast";
