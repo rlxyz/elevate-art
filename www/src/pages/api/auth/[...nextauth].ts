1775105fe796f9bbb11993912f6d0b81f3f3e808
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-20 11:08:27 +0800

    update nextauth secret

--- www/src/pages/api/auth/[...nextauth].ts
+++ www/src/pages/api/auth/[...nextauth].ts
@@ -22,7 +22,7 @@ export const authOptions: NextAuthOptions = {
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: 'Ethereum',
