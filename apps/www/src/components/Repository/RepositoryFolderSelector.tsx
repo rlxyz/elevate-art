3b0805073676912e0883e2771671640649d770e1
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-25 15:59:54 +0800

    fixed overflow issue

--- apps/www/src/components/Repository/RepositoryFolderSelector.tsx
+++ apps/www/src/components/Repository/RepositoryFolderSelector.tsx
@@ -124,7 +124,7 @@ const LayerFolderSelector = () => {
       <aside className='space-y-1'>
         <div className='border border-border rounded-[5px] max-h-[calc(100vh-17.5rem)]'>
           <AnimatePresence>
-            <Reorder.Group axis='y' layoutScroll style={{ overflowY: 'scroll' }} onReorder={setItems} values={items}>
+            <Reorder.Group axis='y' layoutScroll onReorder={setItems} values={items}>
               {items.map((item, index) => {
                 return (
                   <ReorderItem
