9bb8d3d15679a9476b8af8d5b8636abca18ffdd0
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-26 20:30:19 +0800

    added popover for saving/reset state

--- www/src/components/Repository/RepositoryRarityTable.tsx
+++ www/src/components/Repository/RepositoryRarityTable.tsx
@@ -3,7 +3,7 @@ import { Fragment, useCallback, useEffect, useMemo, useRef, useState } from 'rea
 import { Table } from '../Layout/core/Table'
 
 import { Popover, Transition } from '@headlessui/react'
-import { InformationCircleIcon } from '@heroicons/react/outline'
+import { CheckCircleIcon, InformationCircleIcon, RefreshIcon } from '@heroicons/react/outline'
 import useRepositoryStore from '@hooks/store/useRepositoryStore'
 import {
   ColumnDef,
@@ -315,6 +315,47 @@ const RepositoryRuleDisplayView = ({ traitElements, initialSum }: { traitElement
                   </span>
                 )}
               </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-200'
+                enterFrom='opacity-0 translate-y-1'
+                enterTo='opacity-100 translate-y-0'
+                leave='transition ease-in duration-150'
+                leaveFrom='opacity-100 translate-y-0'
+                leaveTo='opacity-0 translate-y-1'
+              >
+                <Popover.Panel className='absolute z-10 w-screen py-6 max-w-xs'>
+                  <div className='overflow-hidden rounded-[5px] bg-lightGray shadow-lg ring-1 ring-black ring-opacity-5'>
+                    {[
+                      {
+                        name: 'Save',
+                        icon: <CheckCircleIcon className='w-4 h-4' />,
+                        onClick: (e) => {
+                          e.preventDefault()
+                        },
+                      },
+                      {
+                        name: 'Reset',
+                        icon: <RefreshIcon className='w-4 h-4' />,
+                        onClick: (e) => {
+                          e.preventDefault()
+                          reset()
+                          setHasFormChange(false)
+                        },
+                      },
+                    ].map(({ name, icon, onClick }) => (
+                      <button
+                        disabled={!hasFormChange}
+                        className='relative items-center p-2 flex space-x-1 disabled:cursor-not-allowed'
+                        onClick={onClick}
+                      >
+                        {icon}
+                        <span className='text-xs'>{name}</span>
+                      </button>
+                    ))}
+                  </div>
+                </Popover.Panel>
+              </Transition>
             </Popover>
           </div>
         ),
