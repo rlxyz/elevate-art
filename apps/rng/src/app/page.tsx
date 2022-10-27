f3d671820401a78544a59616d76c7b1cc5872dc4
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-27 14:27:28 +0800

    now elastic search connection is working

--- apps/rng/src/app/page.tsx
+++ apps/rng/src/app/page.tsx
@@ -1,6 +1,10 @@
+import { getElasticClient } from '../server/elastric-search'
 import MainSearch from './main-search'
 
-export default function Home() {
+export default async function Home() {
+  const elastic = await getElasticClient()
+  const response = await elastic.info()
+  console.log(response.cluster_uuid)
   return (
     <div className='min-h-[calc(100vh-7rem)] flex flex-col items-center justify-center'>
       <MainSearch />
