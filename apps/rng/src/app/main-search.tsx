d899c931f8ad0aad121dac3f7237cf36b9b38088
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-27 18:11:50 +0800

    fixed more build issues

--- apps/rng/src/app/main-search.tsx
+++ apps/rng/src/app/main-search.tsx
@@ -2,19 +2,19 @@
 import { Search } from '@elevateart/ui'
 import clsx from 'clsx'
 
-const graident = [
-  ['from-[#ff3399]', 'to-[#ff3333]'],
-  ['from-[#00ffff]', 'to-success'],
-]
-
 export default function Home() {
+  const graident = [
+    ['from-[#00ffff]', 'to-success'],
+    ['from-[#ff3399]', 'to-[#ff3333]'],
+  ]
   return (
     <>
       <div className='relative group hover:scale-[1.1] transition duration-500'>
         <div
           className={clsx(
             'absolute -inset-1 bg-gradient-to-r rounded-lg blur opacity-25 group-hover:opacity-75 transition duration-1000 group-hover:duration-200',
-            graident[Math.floor(Math.random() * graident.length)].join(' ')
+            // graident[Math.floor(Math.random() * graident.length)].join(' ')
+            graident[0]
           )}
         />
         <Search className='w-full' placeholder='Search the metaverse' focus={false} />
