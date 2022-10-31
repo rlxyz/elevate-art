d05540a3ba2981995c138e63c764051e6a6b8e9a
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-31 22:58:24 +0800

    removed scale up/down of traits when delete

--- www/src/server/router/core/traits.ts
+++ www/src/server/router/core/traits.ts
@@ -1,6 +1,6 @@
 import { deleteImageFromCloudinary } from '@server/common/cld-delete-image'
 import { z } from 'zod'
-import { groupBy, sumBy } from '../../../utils/object-utils'
+import { groupBy } from '../../../utils/object-utils'
 import { createRouter } from '../context'
 
 /**
@@ -14,6 +14,7 @@ export const traitElementRouter = createRouter()
    * Correctness: The invariant (weight) will always be equal to each other due to scale up that occurs.
    *
    * @todo log the delete that occurs
+   * @todo qstash delete traits from cloudinary to ensure it happens
    */
   .mutation('delete', {
     input: z.object({
@@ -24,41 +25,25 @@ export const traitElementRouter = createRouter()
 
       /* Iterate each unique LayerElement & delete many TraitElements  */
       await Promise.all(
-        Object.entries(groupBy(traitElements, (x) => x.layerElementId)).map(
-          async ([layerElementId, traitElementsToBeDeleted]) => {
-            /* Run Atomic Transaction */
-            await ctx.prisma.$transaction(async (tx) => {
-              /* Initialize */
-              const ids = traitElementsToBeDeleted.map((x) => x.id)
+        Object.entries(groupBy(traitElements, (x) => x.layerElementId)).map(async ([_, traitElementsToBeDeleted]) => {
+          /* Run Atomic Transaction */
+          await ctx.prisma.$transaction(async (tx) => {
+            /* Initialize */
+            const ids = traitElementsToBeDeleted.map((x) => x.id)
 
-              /* Sum the weights being deleted */
-              const removed = sumBy(await tx.traitElement.findMany({ where: { id: { in: ids } } }), (x) => x.weight)
-
-              /* Delete the traits */
-              const { count } = await tx.traitElement.deleteMany({
-                where: {
-                  id: {
-                    in: ids,
-                  },
+            /* Delete the traits */
+            await tx.traitElement.deleteMany({
+              where: {
+                id: {
+                  in: ids,
                 },
-              })
-
-              /* Scale the leftover traits  */
-              const toUpdate = await tx.traitElement.findMany({ where: { layerElementId } })
-              const leftover = sumBy(toUpdate, (x) => x.weight)
-              await tx.traitElement.updateMany({
-                where: { id: { in: ids } },
-                data: toUpdate.map(({ id, weight }) => ({
-                  weight: (weight * removed) / leftover,
-                })),
-              })
+              },
             })
-          }
-        )
+          })
+        })
       )
 
       /* Delete many TraitElement from Cloudinary */
-      /* @todo qstash delete traits from cloudinary to ensure it happens */
       await Promise.all(
         traitElements.map(
           async ({ repositoryId: r, layerElementId: l, id: t }) =>
