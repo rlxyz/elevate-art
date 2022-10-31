42a1398309994b9a97c2e3ab05ca9ef0ae3af77f
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-10-31 23:00:39 +0800

    added comments to trait create

--- www/src/server/router/core/traits.ts
+++ www/src/server/router/core/traits.ts
@@ -1,6 +1,5 @@
 import { deleteImageFromCloudinary } from '@server/common/cld-delete-image'
 import { z } from 'zod'
-import { groupBy } from '../../../utils/object-utils'
 import { createRouter } from '../context'
 
 /**
@@ -11,7 +10,6 @@ export const traitElementRouter = createRouter()
   /**
    * Delete TraitElement from their associated LayerElement.
    * This function is dynamic in that it allows a non-sorted list of TraitElements with different associated LayerElements.
-   * Correctness: The invariant (weight) will always be equal to each other due to scale up that occurs.
    *
    * @todo log the delete that occurs
    * @todo qstash delete traits from cloudinary to ensure it happens
@@ -23,25 +21,14 @@ export const traitElementRouter = createRouter()
     async resolve({ ctx, input }) {
       const { traitElements } = input
 
-      /* Iterate each unique LayerElement & delete many TraitElements  */
-      await Promise.all(
-        Object.entries(groupBy(traitElements, (x) => x.layerElementId)).map(async ([_, traitElementsToBeDeleted]) => {
-          /* Run Atomic Transaction */
-          await ctx.prisma.$transaction(async (tx) => {
-            /* Initialize */
-            const ids = traitElementsToBeDeleted.map((x) => x.id)
-
-            /* Delete the traits */
-            await tx.traitElement.deleteMany({
-              where: {
-                id: {
-                  in: ids,
-                },
-              },
-            })
-          })
-        })
-      )
+      /* Delete many TraitElement from Db  */
+      await ctx.prisma.traitElement.deleteMany({
+        where: {
+          id: {
+            in: traitElements.map((x) => x.id),
+          },
+        },
+      })
 
       /* Delete many TraitElement from Cloudinary */
       await Promise.all(
@@ -56,6 +43,12 @@ export const traitElementRouter = createRouter()
       )
     },
   })
+  /**
+   * Creates a TraitElement with their associated LayerElement.
+   * This function is dynamic in that it allows a non-sorted list of TraitElements with different associated LayerElements.
+   *
+   * @todo log the create that occurs
+   */
   .mutation('create', {
     input: z.object({
       traitElements: z.array(z.object({ name: z.string(), layerElementId: z.string(), repositoryId: z.string() })),
