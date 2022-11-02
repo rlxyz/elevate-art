13d58fff4172756c2ff0ae31c8a294fcf349d466
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-02 18:53:32 +0800

    added less clipping on image grid loading

--- www/src/components/Repository/trait-table/trait-grid.tsx
+++ www/src/components/Repository/trait-table/trait-grid.tsx
@@ -22,10 +22,11 @@ const TraitElementGrid: FC<TraitElementGridProps> = ({ traitElements, repository
     <div className={clsx(className, 'grid grid-cols-5 gap-3')}>
       {traitElements.map((trait: TraitElement) => {
         return (
-          <div key={trait.id} className='relative flex-col border border-mediumGrey rounded-[5px] shadow-lg'>
-            <div className='py-4 overflow-hidden'>
+          <div key={trait.id} className='w-auto h-auto flex-col border border-mediumGrey rounded-[5px] shadow-md'>
+            <div className='h-[10rem] w-auto overflow-hidden flex items-center bg-lightGray'>
               <img
-                className='relative w-full h-auto border-t border-b border-mediumGrey'
+                loading='lazy'
+                className='w-auto h-auto border-t border-b border-mediumGrey object-contain bg-white'
                 src={getImageForTrait({
                   r: repositoryId,
                   l: trait.layerElementId,
@@ -33,7 +34,7 @@ const TraitElementGrid: FC<TraitElementGridProps> = ({ traitElements, repository
                 })}
               />
             </div>
-            <div className='px-1 flex flex-col space-y-1 mb-3'>
+            <div className='px-1 py-2 flex flex-col space-y-1'>
               <span className='text-xs font-semibold overflow-hidden w-full'>{truncate(trait.name)}</span>
               <div className='flex flex-col text-[0.6rem]'>
                 <span className='font-semibold overflow-hidden w-full'>
