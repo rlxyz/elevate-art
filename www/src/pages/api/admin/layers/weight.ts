3e3190c09e0ed2d527e2a4d14e3c7c6159ad8bda
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-07 21:57:17 +0800

    cleaned up account page

--- www/src/pages/api/admin/layers/weight.ts
+++ www/src/pages/api/admin/layers/weight.ts
@@ -1,6 +1,7 @@
 import { getLayerElementsWithTraitElementsTotalWeight } from '@server/scripts/get-all-layer-elements-total-weight-'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// @todo authenticate this
 const index = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!prisma) return res.status(400)
   const response = await getLayerElementsWithTraitElementsTotalWeight({ prisma })
