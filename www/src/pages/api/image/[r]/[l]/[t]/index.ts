26142674bd94a533f7fb002fcbfd81879327e60b
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-27 02:18:28 +1100

    fixed image error

--- www/src/pages/api/image/[r]/[l]/[t]/index.ts
+++ www/src/pages/api/image/[r]/[l]/[t]/index.ts
@@ -1,5 +1,5 @@
+import { getCldImgUrl } from '@server/common/cld-get-image'
 import { getServerAuthSession } from '@server/common/get-server-auth-session'
-import { getCldImgUrl } from '@server/utils/cld-image-utils'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const index = async (req: NextApiRequest, res: NextApiResponse) => {
