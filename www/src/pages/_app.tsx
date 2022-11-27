30112bc50964bb2b6e3de8c28a8259284d7129cf
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-27 19:43:44 +1100

    merged

--- www/src/pages/_app.tsx
+++ www/src/pages/_app.tsx
@@ -72,27 +72,27 @@ const ElevateCompilerApp: AppType<{ session: Session | null }> = ({ Component, p
               <OrganisationRouterContext.Provider createStore={() => createOrganisationNavigationStore}>
                 <RepositoryContext.Provider createStore={() => createRepositoryStore}>
                   <DefaultSeo
-                      title='Elevate Art'
-                      description='Design your NFT collection with our leading art generator. Build the perfect collection for your community. Upload your base images, tweak your layers, and algorithmically generate your full collection.'
-                      openGraph={{
-                        type: 'website',
-                        locale: 'en_US',
-                        url: 'https://elevate.art/',
-                        site_name: 'elevate.art',
-                        images: [
-                          {
-                            url: 'https://uploads-ssl.webflow.com/62fb25dec6d6000039acf36b/630df414a98b27db93462c57_Open%20Graph.png',
-                            width: 1600,
-                            height: 840,
-                            alt: 'Elevate Art',
-                          },
-                        ],
-                      }}
-                      twitter={{
-                        handle: '@elevate_art',
-                        cardType: 'summary',
-                      }}
-                    />
+                    title='Elevate Art'
+                    description='Design your NFT collection with our leading art generator. Build the perfect collection for your community. Upload your base images, tweak your layers, and algorithmically generate your full collection.'
+                    openGraph={{
+                      type: 'website',
+                      locale: 'en_US',
+                      url: 'https://elevate.art/',
+                      site_name: 'elevate.art',
+                      images: [
+                        {
+                          url: 'https://uploads-ssl.webflow.com/62fb25dec6d6000039acf36b/630df414a98b27db93462c57_Open%20Graph.png',
+                          width: 1600,
+                          height: 840,
+                          alt: 'Elevate Art',
+                        },
+                      ],
+                    }}
+                    twitter={{
+                      handle: '@elevate_art',
+                      cardType: 'summary',
+                    }}
+                  />
                   <Component {...pageProps} />
                   <Toaster />
                 </RepositoryContext.Provider>
