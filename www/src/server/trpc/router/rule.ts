ed17186e7b05ce9df2ae9888e9055533ec082728
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-27 16:23:43 +1100

    updated ruleRouter

--- www/src/server/trpc/router/rule.ts
+++ www/src/server/trpc/router/rule.ts
@@ -1,31 +1,28 @@
 import { RulesEnum } from 'src/shared/compiler'
 import { z } from 'zod'
-import { createProtectedRouter } from '../context'
+import { protectedProcedure, router } from '../trpc'
 
 /**
  * Rules Router
  * Any TraitElement Rules functionality should implemented here.
  */
-export const rulesRouter = createProtectedRouter()
+export const rulesRouter = router({
   /**
    * Creates a Rule with two associated TraitElements and a "condition" based on the compiler's supported conditions.
    */
-  .mutation('create', {
-    input: z.object({ condition: RulesEnum, traitElements: z.tuple([z.string(), z.string()]) }),
-    async resolve({ ctx, input }) {
+  create: protectedProcedure
+    .input(z.object({ condition: RulesEnum, traitElements: z.tuple([z.string(), z.string()]) }))
+    .mutation(async ({ ctx, input }) => {
       const { condition, traitElements } = input
       return await ctx.prisma.rules.create({
         data: { condition, primaryTraitElementId: traitElements[0], secondaryTraitElementId: traitElements[1] },
       })
-    },
-  })
+    }),
   /**
    * Creates a Rule with two associated TraitElements and a "condition" based on the compiler's supported conditions.
    */
-  .mutation('delete', {
-    input: z.object({ id: z.string() }),
-    async resolve({ ctx, input }) {
-      const { id } = input
-      return await ctx.prisma.rules.delete({ where: { id } })
-    },
-  })
+  delete: protectedProcedure.input(z.object({ ruleId: z.string() })).mutation(async ({ ctx, input }) => {
+    const { ruleId } = input
+    return await ctx.prisma.rules.delete({ where: { id: ruleId } })
+  }),
+})
