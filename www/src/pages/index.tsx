32622d6c75235576d0fa7b16ef00ab1891534f91
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-27 20:04:04 +1100

    fixed index page redirect

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,14 +1,18 @@
 // import { Header } from '@components/layout/Header'
+import { OrganisationNavigationEnum } from '@utils/enums'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { Layout } from 'src/client/components/layout/core/Layout'
 import { Link } from 'src/client/components/layout/Link'
 
 const Home: NextPage = () => {
   const { status } = useSession()
+  const router = useRouter()
+  if (status === 'authenticated') router.push(`/${OrganisationNavigationEnum.enum.Dashboard}`)
   return (
     <Layout>
-      <Layout.Header authenticated={status === 'authenticated'} />
+      <Layout.Header authenticated={false} />
       <Layout.Body>
         <div className='min-h-[calc(100vh-7rem)] space-y-20 h-full flex'>
           <div className='w-full flex flex-col justify-center items-center'>
