d18ff1979ca023261ad30f6387aa4c850c9a4247
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-11-29 16:45:35 +1100

    made version optinal

--- www/src/server/common/cld-get-image.tsx
+++ www/src/server/common/cld-get-image.tsx
@@ -6,7 +6,7 @@ export const IMAGE_QUALITY_SETTINGS: string[] = ['c_scale,w_600', 'q_auto']
 // export const IMAGE_QUALITY_SETTINGS: string[] = []
 export const IMAGE_VERSION = 'v1'
 
-export const getTraitElementImage = ({ r, l, t, version = IMAGE_VERSION }: { r: string; l: string; t: string; version: string }) => {
+export const getTraitElementImage = ({ r, l, t, version = IMAGE_VERSION }: { r: string; l: string; t: string; version?: string }) => {
   return `https://res.cloudinary.com/${env.CLOUDINARY_CLOUD_NAME}/image/upload/${IMAGE_QUALITY_SETTINGS.join('/')}/${version}/${
     env.NEXT_PUBLIC_NODE_ENV
   }/${r}/${l}/${t}.png`
