5d0e7735a34caca3d45cce4946252fd689e868f0
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2022-11-30 13:15:15 +1100

    added maxImageBytesAllowed to health_check

--- www/src/pages/api/health_check.ts
+++ www/src/pages/api/health_check.ts
@@ -3,6 +3,7 @@
 import { getServerAuthSession } from '@server/common/get-server-auth-session'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { log } from 'next-axiom'
+import { formatBytes } from 'src/client/utils/format'
 import { env } from 'src/env/server.mjs'
 
 const index = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,6 +13,7 @@ const index = async (req: NextApiRequest, res: NextApiResponse) => {
     nextAuthUrl: env.NEXTAUTH_URL ? env.NEXTAUTH_URL : process.env.VERCEL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
     apiUrl: env.NEXT_PUBLIC_API_URL,
     appEnv: env.NEXT_PUBLIC_NODE_ENV,
+    maxImageBytesAllowed: formatBytes(env.NEXT_PUBLIC_IMAGE_MAX_BYTES_ALLOWED),
   })
 }
 
