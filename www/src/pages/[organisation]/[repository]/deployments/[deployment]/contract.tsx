bc1d06eab7325070a831673acbebcbc16a4757ca
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-12-07 23:45:06 +1100

    added a disclouser to the contract deployment phase view

--- www/src/pages/[organisation]/[repository]/deployments/[deployment]/contract.tsx
+++ www/src/pages/[organisation]/[repository]/deployments/[deployment]/contract.tsx
@@ -1,5 +1,6 @@
 import RepositoryContractDeploymentCreateModal from '@components/repository/RepositoryDeployment/RepositoryContractDeploymentCreateModal'
 import withOrganisationStore from '@components/withOrganisationStore'
+import { Disclosure } from '@headlessui/react'
 import { CheckCircleIcon, ChevronRightIcon, XCircleIcon } from '@heroicons/react/solid'
 import { useQueryCollectionFindAll } from '@hooks/trpc/collection/useQueryCollectionFindAll'
 import { useQueryLayerElementFindAll } from '@hooks/trpc/layerElement/useQueryLayerElementFindAll'
@@ -126,36 +127,46 @@ const ContractDeploymentPhasesView: FC<{ deployment: RepositoryContractDeploymen
     <div className='space-y-6'>
       <h1 className='text-xl font-bold'>Deployment Status</h1>
       <div>
-        {[ContractDeploymentPhasesEnum.enum.Deploying, ContractDeploymentPhasesEnum.enum.Verification].map((phase, index) => (
-          <div
-            className={clsx(
-              'border-l border-r border-mediumGrey border-b p-4',
-              index === 0 && 'rounded-tl-[5px] rounded-tr-[5px] border-t',
-              index === 2 - 1 && 'rounded-bl-[5px] rounded-br-[5px]'
-            )}
-          >
-            <div className='flex justify-between'>
-              <div className='flex items-center space-x-1'>
-                <ChevronRightIcon className='w-4 h-4' />
-                <h2 className='text-sm font-semibold'>{phase}</h2>
-              </div>
-
-              <div className='flex'>
-                {status === 'FAILED' ? (
-                  <XCircleIcon className='w-5 h-5 text-redError' />
-                ) : (
-                  <>
-                    {status === 'PENDING' && <div className='w-5 h-5 border border-mediumGrey rounded-full' />}
-                    {status === 'VERIFYING' && phase === ContractDeploymentPhasesEnum.enum.Deploying && (
-                      <CheckCircleIcon className='w-5 h-5 text-blueHighlight' />
-                    )}
-                    {status === 'DEPLOYED' && <CheckCircleIcon className='w-5 h-5 text-blueHighlight' />}
-                  </>
+        <div>
+          {[ContractDeploymentPhasesEnum.enum.Deploying, ContractDeploymentPhasesEnum.enum.Verification].map((phase, index) => (
+            <Disclosure>
+              <Disclosure.Button
+                className={clsx(
+                  'border-l border-r border-mediumGrey border-b p-5 w-full',
+                  index === 0 && 'rounded-tl-[5px] rounded-tr-[5px] border-t',
+                  index === 2 - 1 && 'rounded-bl-[5px] rounded-br-[5px]'
                 )}
-              </div>
-            </div>
-          </div>
-        ))}
+              >
+                <div className='flex justify-between'>
+                  <div className='flex items-center space-x-1'>
+                    <ChevronRightIcon className='w-4 h-4' />
+                    <h2 className='text-sm font-semibold'>{phase}</h2>
+                  </div>
+
+                  <div className='flex'>
+                    {status === 'FAILED' ? (
+                      <XCircleIcon className='w-5 h-5 text-redError' />
+                    ) : (
+                      <>
+                        {status === 'PENDING' && <div className='w-5 h-5 border border-mediumGrey rounded-full' />}
+                        {status === 'VERIFYING' && phase === ContractDeploymentPhasesEnum.enum.Deploying && (
+                          <CheckCircleIcon className='w-5 h-5 text-blueHighlight' />
+                        )}
+                        {status === 'DEPLOYED' && <CheckCircleIcon className='w-5 h-5 text-blueHighlight' />}
+                      </>
+                    )}
+                  </div>
+                </div>
+              </Disclosure.Button>
+              <Disclosure.Panel className={clsx('border-l border-r border-b border-mediumGrey p-5')}>
+                <div className='text-xs space-x-6 flex'>
+                  <span className='font-semibold'>{deployment?.createdAt.toLocaleTimeString()}</span>
+                  <p>Address {deployment?.address}</p>
+                </div>
+              </Disclosure.Panel>
+            </Disclosure>
+          ))}
+        </div>
       </div>
     </div>
   )
