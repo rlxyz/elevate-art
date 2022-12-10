2c6520324012d24f44800a331fa1da0176db25fb
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2022-12-11 01:15:28 +1100

    done with base implementation of gallery

--- apps/mint/src/pages/[address]/gallery.tsx
+++ apps/mint/src/pages/[address]/gallery.tsx
@@ -1,23 +1,14 @@
 import { MintLayout } from '@Components/core/MintLayout'
 import { Layout, LayoutContainer } from '@Components/layout/core/Layout'
+import LinkComponent from '@Components/layout/link/Link'
 import { RepositoryContractDeploymentStatus } from '@prisma/client'
-import type { BigNumber } from 'ethers'
 import LogRocket from 'logrocket'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import * as InfiniteScrollComponent from 'react-infinite-scroll-component'
 import { useQueryContractDeployment } from 'src/client/hooks/useQueryContractDeployment'
 import { useAccount } from 'wagmi'
-
-// @todo find somewhere to put this
-export type ContractData = {
-  projectOwner: string
-  ethPrice: BigNumber
-  maxAllocationPerAddress: BigNumber
-  totalSupply: BigNumber
-  presaleStartTime: Date
-  publicStartTime: Date
-}
 
 export const HomePage = () => {
   const router = useRouter()
@@ -29,6 +20,33 @@ export const HomePage = () => {
       LogRocket.identify(account?.address)
     }
   }, [account?.address])
+
+  const [displayLength, setDisplayLength] = useState<number>(0)
+  const [hasMore, setHasMore] = useState(true)
+
+  useEffect(() => {
+    reset()
+    fetch()
+  }, [current?.deployment?.address])
+
+  const fetch = () => {
+    setDisplayLength((prev) => prev + 25)
+  }
+
+  const reset = () => {
+    setDisplayLength(0)
+    setHasMore(true)
+  }
+
+  const fetchMoreData = () => {
+    if (!current) return
+    if (!current.deployment?.repositoryDeployment) return
+    if (displayLength + 25 > current.deployment.repositoryDeployment.collectionTotalSupply) {
+      setHasMore(false)
+      return
+    }
+    return fetch()
+  }
 
   if (!current || !current.deployment || !(current.deployment.status === RepositoryContractDeploymentStatus.DEPLOYED)) return <></>
 
@@ -55,24 +73,47 @@ export const HomePage = () => {
             contractData={contractData}
           />
           <LayoutContainer border='none'>
-            <div className='grid grid-cols-4 gap-6 py-6'>
-              {Array.from(Array(20).keys()).map((item) => (
-                <div key={item} className='border border-mediumGrey rounded-[5px]'>
-                  <Image
-                    src={`${'http://localhost:3000'}/api/asset/${deployment.repository.organisation.name}/${deployment.repository.name}/${
-                      deployment.repositoryDeployment?.name
-                    }/${item}`}
-                    width={300}
-                    height={300}
-                    alt='some-id'
-                    className='object-cover m-auto rounded-t-[5px]'
-                  />
-                  <div className='p-2'>
-                    <h1 className='text-xs font-semibold italic'>{item}</h1>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <InfiniteScrollComponent.default
+              dataLength={displayLength}
+              next={() => {
+                fetchMoreData()
+              }}
+              hasMore={hasMore}
+              loader={<></>}
+            >
+              <div className='grid grid-cols-4 gap-6 py-6'>
+                {/** @todo should use current.contract.totalSupply */}
+                {Array.from(Array(current.deployment.repositoryDeployment?.collectionTotalSupply).keys())
+                  .slice(0, displayLength)
+                  .map((item) => (
+                    <div key={item} className='border border-mediumGrey rounded-[5px]'>
+                      <Image
+                        src={`${'http://localhost:3000'}/api/asset/${deployment.repository.organisation.name}/${
+                          deployment.repository.name
+                        }/${deployment.repositoryDeployment?.name}/${item}`}
+                        width={300}
+                        height={300}
+                        alt='some-id'
+                        className='object-cover m-auto rounded-t-[5px]'
+                      />
+                      <div className='p-2'>
+                        <h1 className='text-xs font-semibold italic'>
+                          <LinkComponent
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href={`${'http://localhost:3000'}/api/asset/${deployment.repository.organisation.name}/${
+                              deployment.repository.name
+                            }/${deployment.repositoryDeployment?.name}/${item}/metadata`}
+                            underline
+                          >
+                            {item}
+                          </LinkComponent>
+                        </h1>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </InfiniteScrollComponent.default>
           </LayoutContainer>
         </MintLayout>
       </Layout.Body>
