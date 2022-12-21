ed0792a625fef1fe9cc7fa1b855af15b4d211372
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2022-12-21 16:14:14 +1100

    added settings for contracts and fixed links

---
+++ www/src/client/components/contractDeployment/ContractSettings.tsx
@@ -0,0 +1,212 @@
+import { NextLinkWithHoverHueComponent } from '@components/layout/link/NextLinkWithHoverHueComponent'
+import { useContractCreationStore } from '@hooks/store/useContractCreationStore'
+import { useQueryOrganisationFindAll } from '@hooks/trpc/organisation/useQueryOrganisationFindAll'
+import { title } from 'process'
+import type { FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { capitalize } from 'src/client/utils/format'
+import type { ContractDetailsForm } from './ContactCreationForms/ContactDetailsForm'
+import { ContractForm } from './ContractForm'
+
+export const SettingsNavigations: FC<{ routes: { name: string; href: string; selected: boolean }[] }> = ({ routes }) => {
+  return (
+    <div>
+      {routes.map(({ name, href, selected }) => {
+        return (
+          <NextLinkWithHoverHueComponent key={name} href={href} enabled={selected}>
+            {capitalize(name)}
+          </NextLinkWithHoverHueComponent>
+        )
+      })}
+    </div>
+  )
+}
+
+export const ContractGeneralSettings = () => {
+  const { current: organisation } = useQueryOrganisationFindAll()
+
+  const {
+    currentSegment,
+    contractName,
+    contractSymbol,
+    mintType,
+    blockchain,
+    artCollection,
+    setContractName,
+    setContractSymbol,
+    setMintType,
+    setBlockchain,
+    setCurrentSegment,
+    setArtCollection,
+  } = useContractCreationStore()
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      contractName: contractName,
+      contractSymbol: contractSymbol,
+      mintType: mintType,
+      blockchain: blockchain,
+      artCollection: artCollection,
+    },
+  })
+
+  const onSubmit = ({ contractName, contractSymbol, mintType, blockchain, artCollection }: ContractDetailsForm) => {
+    setContractName(contractName)
+    setContractSymbol(contractSymbol)
+    setMintType(mintType)
+    setBlockchain(blockchain)
+    setArtCollection(artCollection)
+    setCurrentSegment(currentSegment + 1)
+  }
+
+  const localContractName = watch('contractName')
+  const localContractSymbol = watch('contractSymbol')
+  const localArtCollection = watch('artCollection')
+  const localBlockchain = watch('blockchain')
+
+  return (
+    <ContractForm>
+      <ContractForm.Header title={title} description={`description`} />
+      <ContractForm.Body onSubmit={handleSubmit(onSubmit)}>
+        <div className='w-full '>
+          <ContractForm.Body.Input
+            {...register('contractName', {
+              required: true,
+              maxLength: {
+                value: 20,
+                message: 'Max length is 20',
+              },
+              minLength: {
+                value: 3,
+                message: 'Max length is 3',
+              },
+              pattern: /^[-/a-z0-9 ]+$/gi,
+              onChange: (e) => {
+                setValue('contractName', e.target.value)
+              },
+            })}
+            label={'Contract Name'}
+            description={'Your contract name on websites like Etherscan'}
+            className='col-span-4'
+            placeholder={contractName}
+            error={errors.contractName}
+            maxLength={20}
+          />
+
+          <ContractForm.Body.Input
+            {...register('contractSymbol', {
+              required: true,
+              maxLength: {
+                value: 6,
+                message: 'Max length is 6',
+              },
+              minLength: {
+                value: 3,
+                message: 'Min length is 3',
+              },
+
+              pattern: /^[-/a-z0-9 ]+$/gi,
+              onChange: (e) => {
+                setValue('contractSymbol', e.target.value.toUpperCase())
+              },
+            })}
+            label={'Symbol'}
+            description={'The name of the token on Etherscan'}
+            className='col-span-2'
+            placeholder={contractSymbol}
+            error={errors.contractSymbol}
+            maxLength={6}
+          />
+          <ContractForm.Body.Select
+            {...register('artCollection', {
+              required: true,
+              onChange: (e) => {
+                setValue('artCollection', e.target.value)
+              },
+            })}
+            label={'Art Collection'}
+            description={`Select which collection you're using from Elevate Art`}
+            className='col-span-6'
+            error={errors.artCollection}
+            defaultValue={artCollection}
+          >
+            <option value={'main'}>{'main'}</option>
+            <option value={'development'}>{'development'}</option>
+          </ContractForm.Body.Select>
+
+          <div className='col-span-3 flex flex-col'>
+            <label className='text-xs font-semibold'>Mint Type</label>
+            <div className='flex space-x-3'>
+              <div className='h-full flex items-center space-x-2'>
+                <ContractForm.Body.Radio
+                  className=''
+                  description=''
+                  {...register('mintType', {
+                    onChange: (e) => {
+                      setValue('mintType', 'off-chain')
+                    },
+                  })}
+                  label={'Off-Chain'}
+                  error={errors.mintType}
+                  checked={true}
+                />
+              </div>
+              <div className='h-full flex items-center space-x-2'>
+                <ContractForm.Body.Radio
+                  className=''
+                  description=''
+                  {...register('mintType', {
+                    onChange: (e) => {
+                      setValue('mintType', 'on-chain')
+                    },
+                  })}
+                  label={'On-Chain'}
+                  disabled={true}
+                  error={errors.mintType}
+                />
+                <span className='flex px-2 py-1 items-center rounded-full bg-lightGray bg-opacity-40 border border-mediumGrey text-[0.6rem] font-medium text-black h-fit'>
+                  <span className='text-darkGrey text-[0.6rem]'>Soon</span>
+                </span>
+              </div>
+            </div>
+            <p className='text-[0.6rem] text-darkGrey'>Select the type of mint for the art generation of this collection.</p>
+          </div>
+
+          <ContractForm.Body.Select
+            {...register('blockchain', {
+              required: true,
+              onChange: (e) => {
+                setValue('blockchain', e.target.value)
+              },
+            })}
+            label={'Blockchain'}
+            description={'Select which blockchain you’re launching your NFT collection on'}
+            className='col-span-6'
+            error={errors.blockchain}
+            defaultValue={blockchain}
+          >
+            <option value={'goerli'}>{capitalize('goerli')}</option>
+            <option value={'mainnet'}>{capitalize('mainnet')}</option>
+          </ContractForm.Body.Select>
+        </div>
+        <div>
+          <ContractForm.Body.Summary
+            contractName={localContractName}
+            contractSymbol={localContractSymbol}
+            blockchain={localBlockchain}
+            mintType={mintType}
+            artCollection={localArtCollection}
+            currentSegment={currentSegment}
+          />
+        </div>
+      </ContractForm.Body>
+    </ContractForm>
+  )
+}
---
+++ www/src/pages/create/[organisation]/[repository]/deployments/[deployment]/settings/deploy.tsx
@@ -0,0 +1,152 @@
+import AppRoutesNavbar, { ZoneRoutesNavbarPopover } from '@components/layout/header/AppRoutesNavbarProps'
+import { PageRoutesNavbar } from '@components/layout/header/PageRoutesNavbar'
+import { TriangleIcon } from '@components/layout/icons/RectangleGroup'
+import { OrganisationRoutesNavbarPopover } from '@components/organisation/OrganisationRoutesNavbar'
+import withOrganisationStore from '@components/withOrganisationStore'
+import { CubeIcon, GlobeAltIcon } from '@heroicons/react/outline'
+import { useQueryOrganisationFindAll } from '@hooks/trpc/organisation/useQueryOrganisationFindAll'
+import { useQueryRepositoryFindByName } from '@hooks/trpc/repository/useQueryRepositoryFindByName'
+import { useQueryRepositoryContractDeployment } from '@hooks/trpc/repositoryContractDeployment/useQueryRepositoryDeployments'
+import { useQueryRepositoryDeployments } from '@hooks/trpc/repositoryDeployment/useQueryRepositoryDeployments'
+import type { NextPage } from 'next'
+import { Layout } from 'src/client/components/layout/core/Layout'
+import { OrganisationAuthLayout } from 'src/client/components/organisation/OrganisationAuthLayout'
+import { SettingsNavigations } from 'src/client/components/organisation/OrganisationSettings'
+import { capitalize } from 'src/client/utils/format'
+import { env } from 'src/env/client.mjs'
+import {
+  CollectionNavigationEnum,
+  ContractSettingsNavigationEnum,
+  DeploymentNavigationEnum,
+  OrganisationNavigationEnum,
+  ZoneNavigationEnum,
+} from 'src/shared/enums'
+
+const Page: NextPage = () => {
+  const { current: organisation } = useQueryOrganisationFindAll()
+  const { all: contractDeployment } = useQueryRepositoryContractDeployment()
+  const { current: deployment, isLoading: isLoading } = useQueryRepositoryDeployments()
+  const { current: repository } = useQueryRepositoryFindByName()
+  return (
+    <OrganisationAuthLayout route={OrganisationNavigationEnum.enum.Settings}>
+      <Layout>
+        <Layout.AppHeader>
+          <AppRoutesNavbar>
+            <AppRoutesNavbar.Item label={capitalize(ZoneNavigationEnum.enum.Create)} href={`/${ZoneNavigationEnum.enum.Create}`}>
+              <ZoneRoutesNavbarPopover
+                title='Apps'
+                routes={[
+                  {
+                    label: capitalize(ZoneNavigationEnum.enum.Dashboard),
+                    href: `/${ZoneNavigationEnum.enum.Dashboard}`,
+                    selected: false,
+                    icon: (props: any) => <CubeIcon className='w-4 h-4' />,
+                  },
+                  {
+                    label: capitalize(ZoneNavigationEnum.enum.Create),
+                    href: `/${ZoneNavigationEnum.enum.Create}`,
+                    selected: false,
+                    icon: (props: any) => <TriangleIcon className='w-4 h-4' />,
+                  },
+                  {
+                    label: capitalize(ZoneNavigationEnum.enum.Explore),
+                    href: `/${ZoneNavigationEnum.enum.Explore}`,
+                    selected: true,
+                    icon: (props: any) => <GlobeAltIcon className='w-4 h-4' />,
+                  },
+                ]}
+              />
+            </AppRoutesNavbar.Item>
+            <AppRoutesNavbar.Item
+              label={organisation?.name || ''}
+              href={`/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}`}
+            >
+              <OrganisationRoutesNavbarPopover />
+            </AppRoutesNavbar.Item>
+            <AppRoutesNavbar.Item
+              label={repository?.name || ''}
+              href={`/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}`}
+            />
+            <AppRoutesNavbar.Item
+              label={deployment?.name || ''}
+              href={`/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}`}
+            />
+          </AppRoutesNavbar>
+        </Layout.AppHeader>
+        <Layout.PageHeader>
+          <PageRoutesNavbar>
+            {[
+              {
+                name: DeploymentNavigationEnum.enum.Deployment,
+                href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}`,
+                enabled: false,
+                loading: isLoading,
+              },
+              {
+                name: DeploymentNavigationEnum.enum.Contract,
+                href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Contract}`,
+                enabled: false,
+                loading: isLoading,
+              },
+              {
+                name: DeploymentNavigationEnum.enum.Settings,
+                href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}`,
+                enabled: true,
+                loading: false,
+              },
+            ].map((item) => (
+              <PageRoutesNavbar.Item key={item.name} opts={item} />
+            ))}
+          </PageRoutesNavbar>
+        </Layout.PageHeader>
+        <Layout.Body>
+          <div className='py-8 space-y-8'>
+            <div className='grid grid-cols-10 gap-x-6'>
+              <div className='col-span-2'>
+                <SettingsNavigations
+                  routes={[
+                    {
+                      name: ContractSettingsNavigationEnum.enum.Details,
+                      href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}`,
+                      selected: false,
+                    },
+                    {
+                      name: ContractSettingsNavigationEnum.enum.Mechanics,
+                      href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}/${ContractSettingsNavigationEnum.enum.Mechanics}`,
+                      selected: false,
+                    },
+                    {
+                      name: ContractSettingsNavigationEnum.enum.Revenue,
+                      href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}/${ContractSettingsNavigationEnum.enum.Revenue}`,
+                      selected: false,
+                    },
+                    {
+                      name: ContractSettingsNavigationEnum.enum.Allowlist,
+                      href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}/${ContractSettingsNavigationEnum.enum.Allowlist}`,
+                      selected: false,
+                    },
+                    {
+                      name: ContractSettingsNavigationEnum.enum.Deploy,
+                      href: `/${env.NEXT_PUBLIC_CREATE_CLIENT_BASE_PATH}/${organisation?.name}/${repository?.name}/${CollectionNavigationEnum.enum.Deployments}/${deployment?.name}/${DeploymentNavigationEnum.enum.Settings}/${ContractSettingsNavigationEnum.enum.Deploy}`,
+                      selected: false,
+                    },
+                  ]}
+                />
+              </div>
+              <div className='col-span-8'>
+                <div className='space-y-6'>
+                  <h1>Deploy</h1>
+                  {/* <OrganisationTeamAddUser />
+                  <OrganisationTeamDisplayUsers />
+                  <OrganisationTeamDisplayPending /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Layout.Body>
+      </Layout>
+    </OrganisationAuthLayout>
+  )
+}
+
+export default withOrganisationStore(Page)
