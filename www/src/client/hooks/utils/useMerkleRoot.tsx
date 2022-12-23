e43c4eb32ef33880d34af2d1610e4ef0bf4ee4ef
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2022-12-23 14:16:20 +1100

    addded new variables and db schema

--- www/src/client/hooks/utils/useMerkleRoot.tsx
+++ www/src/client/hooks/utils/useMerkleRoot.tsx
@@ -1,4 +1,3 @@
-import { claimConfig, presaleConfig } from '@Utils/merkle_roots'
 import { ethers } from 'ethers'
 import keccak256 from 'keccak256'
 import MerkleTree from 'merkletreejs'
@@ -9,6 +8,22 @@ import MerkleTree from 'merkletreejs'
  * @param {string} value of airdrop tokens to claimee
  * @returns {Buffer} Merkle Tree node
  */
+
+type IWhitelist = {
+  whitelist: Record<string, number>
+}
+
+// Config from generator
+const presaleConfig: IWhitelist = {
+  whitelist: {
+    '0xcd3B766CCDd6AE721141F452C550Ca635964ce71': 1,
+    '0x90F79bf6EB2c4f870365E785982E1f101E93b906': 1,
+    '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199': 1,
+    '0xdD2FD4581271e230360230F9337D5c0430Bf44C0': 1,
+    '0x71bE63f3384f5fb98995898A86B02Fb2426c5788': 1,
+  },
+}
+
 export const generateLeaf = (address: string, value: string): Buffer => {
   return Buffer.from(
     // Hash in appropriate Merkle format
@@ -26,10 +41,10 @@ export const presaleMerkleTree = new MerkleTree(
   { sortPairs: true }
 )
 
-export const claimMerkleTree = new MerkleTree(
-  // Generate leafs
-  Object.entries(claimConfig.whitelist).map(([address, tokens]) => generateLeaf(ethers.utils.getAddress(address), tokens.toString())),
-  // Hashing function
-  keccak256,
-  { sortPairs: true }
-)
+// export const claimMerkleTree = new MerkleTree(
+//   // Generate leafs
+//   Object.entries(claimConfig.whitelist).map(([address, tokens]) => generateLeaf(ethers.utils.getAddress(address), tokens.toString())),
+//   // Hashing function
+//   keccak256,
+//   { sortPairs: true }
+// )
