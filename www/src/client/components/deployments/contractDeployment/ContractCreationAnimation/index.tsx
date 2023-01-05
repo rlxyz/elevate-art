820b2543e872b3a40c67cc4da9648a8f86559799
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2023-01-05 17:41:35 +1100

    modularised the constant variabls for the contract creation and animation

--- www/src/client/components/deployments/contractDeployment/ContractCreationAnimation/index.tsx
+++ www/src/client/components/deployments/contractDeployment/ContractCreationAnimation/index.tsx
@@ -1,63 +1,63 @@
 import useContractCreationStore from '@components/deployments/contractDeployment/ContactCreationForm/useContractCreationStore'
-import { TriangleIcon } from '@components/layout/icons/RectangleGroup'
-import { ChevronLeftIcon, ChevronRightIcon, CubeIcon, MoonIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import clsx from 'clsx'
 import { AnimatePresence } from 'framer-motion'
 import type { FC } from 'react'
-import { ContractCreationEnum } from '../ContactCreationForm'
+import type { ContractCreationAnimationProps } from '..'
 import { ButtonWithSelector } from './ButtonWithSelector'
 import { CarouselSegment } from './CarouselSegment'
 import { LineWithGradient } from './LineWithGradient'
 import { useAnimationMotionValues } from './useAnimationMotionValues'
 
 /** @todo modularise */
-export const ContractCreationHelperAnimation: FC<{ className: string }> = ({ className }) => {
+export const ContractCreationHelperAnimation: FC<{
+  className: string
+  contractCreationAnimation: ContractCreationAnimationProps[]
+}> = ({ className, contractCreationAnimation }) => {
   const { handleClick } = useAnimationMotionValues()
   const { currentSegment } = useContractCreationStore()
   return (
     <div className={clsx('flex h-full flex-col items-center w-full space-y-9', className)}>
       <div className='relative my-2 flex h-20 w-full items-center overflow-x-hidden rounded-[5px]'>
         <AnimatePresence>
-          <CarouselSegment
-            enabled={currentSegment === ContractCreationEnum.enum.ContractDetails}
-            transformOutputRange={['15%', '32.5%', '50%']}
-            transformInputRange={[0, 0.25, 0.5]}
-            index={0}
-            x={0.5}
-            opacity={1}
-            onClick={handleClick}
-          >
-            <TriangleIcon className='w-8 h-8 -translate-y-[1.5px] text-black' />
-          </CarouselSegment>
-          <CarouselSegment
-            enabled={currentSegment === ContractCreationEnum.enum.MintDetails}
-            transformOutputRange={['32.5%', '50%', '67.5%']}
-            transformInputRange={[0.25, 0.5, 0.75]}
-            index={1}
-            x={0.75}
-            opacity={0.5}
-            onClick={handleClick}
-          >
-            <CubeIcon className='w-10 h-10 text-black' />
-          </CarouselSegment>
-          <CarouselSegment
-            enabled={currentSegment === ContractCreationEnum.enum.ContractCompletion}
-            transformOutputRange={['50%', '67.5%', '85%']}
-            transformInputRange={[0.5, 0.75, 1]}
-            index={2}
-            x={1}
-            opacity={0.25}
-            onClick={handleClick}
-          >
-            <MoonIcon className='w-10 h-10 text-black' />
-          </CarouselSegment>
+          {contractCreationAnimation.map((item) => (
+            <CarouselSegment
+              key={item.id}
+              enabled={currentSegment === item.id}
+              transformOutputRange={item.transformOutputRange}
+              transformInputRange={item.transformInputRange}
+              index={item.index}
+              x={item.motionX}
+              id={item.id}
+              opacity={item.motionOpacity}
+              onClick={handleClick}
+            >
+              {item.icon}
+            </CarouselSegment>
+          ))}
         </AnimatePresence>
 
-        <ButtonWithSelector onClick={() => handleClick(currentSegment - 1)} enabled={currentSegment === 0}>
+        <ButtonWithSelector
+          onClick={() => {
+            const segment = contractCreationAnimation.find((item) => item.id === currentSegment)
+            if (!segment) return
+            if (!segment.previous) return
+            handleClick(segment.previous)
+          }}
+          enabled={contractCreationAnimation.find((item) => item.id === currentSegment)?.previous !== undefined}
+        >
           <ChevronLeftIcon className='w-4 h-4 text-darkGrey' />
         </ButtonWithSelector>
         <LineWithGradient />
-        <ButtonWithSelector onClick={() => handleClick(currentSegment + 1)} enabled={currentSegment === 2}>
+        <ButtonWithSelector
+          onClick={() => {
+            const segment = contractCreationAnimation.find((item) => item.id === currentSegment)
+            if (!segment) return
+            if (!segment.next) return
+            handleClick(segment.next)
+          }}
+          enabled={contractCreationAnimation.find((item) => item.id === currentSegment)?.next !== undefined}
+        >
           <ChevronRightIcon className='w-4 h-4 text-darkGrey' />
         </ButtonWithSelector>
       </div>
--- www/src/client/components/deployments/contractDeployment/index.tsx
+++ www/src/client/components/deployments/contractDeployment/index.tsx
@@ -1,15 +1,88 @@
-import { ContractCreationForm } from '@components/deployments/contractDeployment/ContactCreationForm'
+import type { ContractCreationType, ContractFormProps } from '@components/deployments/contractDeployment/ContactCreationForm'
+import { ContractCreationEnum, ContractCreationForm } from '@components/deployments/contractDeployment/ContactCreationForm'
 import {
   ContractContext,
   createContractCreationStore,
 } from '@components/deployments/contractDeployment/ContactCreationForm/useContractCreationStore'
-import { ContractCreationHelperAnimation } from '@components/deployments/contractDeployment/ContractCreationAnimation'
+import { TriangleIcon } from '@components/layout/icons/RectangleGroup'
+import { CubeIcon, MoonIcon } from '@heroicons/react/outline'
+import type { FC } from 'react'
+import { ContractDetailsForm } from './ContactCreationForm/ContactDetailsForm'
+import { ContractCompletionForm } from './ContactCreationForm/ContractCompletionForm'
+import { MintDetailsForm } from './ContactCreationForm/MintDetailsForm'
+import { ContractCreationHelperAnimation } from './ContractCreationAnimation'
+
+export interface ContractCreationAnimationProps extends ContractCreationSegmentProps {
+  icon: React.ReactNode // animation
+  motionX: number // animation
+  motionOpacity: number // animation
+  transformInputRange: number[] // animation
+  transformOutputRange: string[] // animation
+}
+
+export interface ContractCreationFormProps extends ContractCreationSegmentProps {
+  title: string // form
+  description: string // form
+  component: FC<ContractFormProps> // form
+}
+
+export interface ContractCreationSegmentProps {
+  index: number // animation
+  id: ContractCreationType // form & animation
+  previous: ContractCreationType | null // animation & form
+  next: ContractCreationType | null // animation & form
+}
+
+export const ContractCreationSegments: (ContractCreationAnimationProps & ContractCreationFormProps)[] = [
+  {
+    index: 0,
+    id: ContractCreationEnum.enum.ContractDetails,
+    title: 'Smart Contract Details',
+    description: 'These are important terms for your contract that you need to finalise before continuing!',
+    component: ContractDetailsForm,
+    motionX: 0.5,
+    motionOpacity: 1,
+    icon: <TriangleIcon className='w-10 h-10 text-darkGrey' />,
+    previous: null,
+    next: ContractCreationEnum.enum.MintDetails,
+    transformOutputRange: ['15%', '32.5%', '50%'],
+    transformInputRange: [0, 0.25, 0.5],
+  },
+  {
+    index: 1,
+    id: ContractCreationEnum.enum.MintDetails,
+    title: 'Mint Details',
+    description: 'Enter the details of your mint',
+    motionX: 0.75,
+    motionOpacity: 0.5,
+    component: MintDetailsForm,
+    icon: <CubeIcon className='w-10 h-10 text-darkGrey' />,
+    previous: ContractCreationEnum.enum.ContractDetails,
+    next: ContractCreationEnum.enum.ContractCompletion,
+    transformOutputRange: ['32.5%', '50%', '67.5%'],
+    transformInputRange: [0.25, 0.5, 0.75],
+  },
+  {
+    index: 2,
+    id: ContractCreationEnum.enum.ContractCompletion,
+    title: 'Mint Revenue Splits',
+    description: 'Enter the details of your mint',
+    motionX: 1,
+    motionOpacity: 0.25,
+    component: ContractCompletionForm,
+    icon: <MoonIcon className='w-10 h-10 text-darkGrey' />,
+    previous: ContractCreationEnum.enum.MintDetails,
+    next: null,
+    transformOutputRange: ['50%', '67.5%', '85%'],
+    transformInputRange: [0.5, 0.75, 1],
+  },
+]
 
 export const ContractCreation = () => {
   return (
     <ContractContext.Provider createStore={() => createContractCreationStore}>
-      <ContractCreationHelperAnimation className='py-16' />
-      <ContractCreationForm className='h-[calc(100vh-17.75rem)] py-8' />
+      <ContractCreationHelperAnimation contractCreationAnimation={ContractCreationSegments} className='py-16' />
+      <ContractCreationForm contractCreationForm={ContractCreationSegments} className='h-[calc(100vh-17.75rem)] py-8' />
     </ContractContext.Provider>
   )
 }
