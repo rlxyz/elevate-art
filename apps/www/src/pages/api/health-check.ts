267f3980a6ac59c763041a3c19e6ff580766a94f
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2023-01-23 16:34:04 +1100

    pushed new vercel url

--- apps/www/src/pages/api/health-check.ts
+++ apps/www/src/pages/api/health-check.ts
@@ -10,7 +10,7 @@ const index = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerAuthSession({ req, res })
   log.debug(`new health check request`, { user: session?.user?.id || 'anonymous' })
   return res.status(200).send({
-    nextAuthUrl: env.NEXTAUTH_URL,
+    nextAuthUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : env.NEXTAUTH_URL,
     apiUrl: env.NEXT_PUBLIC_API_URL,
     assetUrl: env.NEXT_PUBLIC_ASSET_URL,
     nodeEnv: env.NODE_ENV,
