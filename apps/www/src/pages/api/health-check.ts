84d316bccf79f87e8a3934fe7e04fb59d6aad4e2
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2023-01-23 17:03:28 +1100

    added initial networkId

--- apps/www/src/pages/api/health-check.ts
+++ apps/www/src/pages/api/health-check.ts
@@ -1,6 +1,7 @@
 import { AssetDeploymentBranch } from '@prisma/client'
 import { getServerAuthSession } from '@server/common/get-server-auth-session'
 import { getLayerDeploymentBucketName, getTokenDeploymentBucketName } from '@server/utils/gcp-storage'
+import { parseChainId } from '@utils/ethers'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { log } from 'next-axiom'
 import { formatBytes } from 'src/client/utils/format'
@@ -19,6 +20,7 @@ const index = async (req: NextApiRequest, res: NextApiResponse) => {
     bucketLayer: getLayerDeploymentBucketName(),
     bucketTokenPreview: getTokenDeploymentBucketName({ branch: AssetDeploymentBranch.PREVIEW }),
     bucketTokenProduction: getTokenDeploymentBucketName({ branch: AssetDeploymentBranch.PRODUCTION }),
+    networkId: parseChainId(env.NEXT_PUBLIC_NETWORK_ID),
   })
 }
 
