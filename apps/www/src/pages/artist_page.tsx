5fb0271634b3fc2e14ce0ce95f2e104bd5f9bb49
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2023-01-31 11:59:08 +1100

    added dropdown for artist and description

--- apps/www/src/pages/artist_page.tsx
+++ apps/www/src/pages/artist_page.tsx
@@ -2,11 +2,14 @@
 import AvatarComponent from '@components/layout/avatar/Avatar'
 import { Layout } from '@components/layout/core/Layout'
 import { default as NextLink, default as NextLinkComponent } from '@components/layout/link/NextLink'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon, DocumentDuplicateIcon } from '@heroicons/react/outline'
 import { OrganisationNavigationEnum } from '@utils/enums'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { Fragment } from 'react'
 import { routeBuilder } from 'src/client/utils/format'
 
 const Profile = () => {
@@ -23,7 +26,7 @@ const Profile = () => {
         <div className='mx-auto max-w-5xl px-4 sm:px-6 lg:px-8'>
           <div className='-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5'>
             <div className='flex'>
-              <img className='h-24 w-24 rounded-full ring-8 ring-white sm:h-32 sm:w-32' src='/images/avatar-blank.png' alt='' />
+              <AvatarComponent className='h-24 w-24 rounded-full ring-8 ring-white sm:h-32 sm:w-32' src='/images/avatar-blank.png' />
             </div>
           </div>
           <div className='mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1'>
@@ -39,22 +42,28 @@ const Profile = () => {
               </div>
             </div>
           </div>
+
+          <ArtistDropdown />
+
           <button
             type='button'
             className='inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2'
           >
-            {/* <EnvelopeIcon className='-ml-1 mr-2 h-5 w-5 text-gray-400' aria-hidden='true' /> */}
-            <span>dreamlab.eth</span>
-          </button>
-          <button
-            type='button'
-            className='inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2'
-          >
-            {/* <PhoneIcon className='-ml-1 mr-2 h-5 w-5 text-gray-400' aria-hidden='true' /> */}
             <span>Follow</span>
           </button>
           <div className='mt-6 hidden min-w-0 flex-1 sm:block md:hidden'>
             <h1 className='truncate text-2xl font-bold text-gray-900'>NAME</h1>
+          </div>
+          <div className='py-6 text-darkGrey'>Australian based photographer showcasing otherworldly colours and textures.</div>
+          <div className='flex space-x-4'>
+            <div>
+              <AvatarComponent variant='sm' className='ring-4 ring-white ' src='/images/avatar-blank.png' />
+              <AvatarComponent variant='sm' className='ring-4 ring-white ' src='/images/avatar-blank.png' />
+              <AvatarComponent variant='sm' className='ring-4 ring-white ' src='/images/avatar-blank.png' />
+              <AvatarComponent variant='sm' className='ring-4 ring-white ' src='/images/avatar-blank.png' />
+              <AvatarComponent variant='sm' className='ring-4 ring-white ' src='/images/avatar-blank.png' />
+            </div>
+            <div className='font-semibold'> 14 collectors</div>
           </div>
         </div>
       </div>
@@ -241,5 +250,141 @@ const MintButton = () => {
         <span className='text-sm'>View Mint</span>
       </div>
     </NextLinkComponent>
+  )
+}
+
+const ArtistDropdown = () => {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  return (
+    <Menu as='div' className='relative inline-block text-left'>
+      <div>
+        <Menu.Button className='inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100'>
+          dreamlab.eth
+          <ChevronDownIcon className='-mr-1 ml-2 h-5 w-5' aria-hidden='true' />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
+      >
+        <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          <div className='py-1'>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Edit
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Duplicate
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className='py-1'>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Archive
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Move
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className='py-1'>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Share
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Add to favorites
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className='py-1'>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href='#'
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'group flex items-center px-4 py-2 text-sm'
+                  )}
+                >
+                  <DocumentDuplicateIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+                  Delete
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
