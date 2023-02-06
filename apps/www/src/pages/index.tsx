f74a1f4f0c2728bb381c6299e2c6cf95aabdf28f
jeevan.eth <33827489+jeevanpillay@users.noreply.github.com>
2023-02-06 11:02:44 +1100

    729 minted

--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -69,7 +69,7 @@ const Mint = () => {
           <div className='flex space-x-9'>
             <div>
               <div className='text-xs text-darkGrey'>Current Minted</div>
-              <div className='text-3xl font-bold'>695/1333</div>
+              <div className='text-3xl font-bold'>729/1333</div>
             </div>
             <div>
               <div className='text-xs text-darkGrey'>Phase</div>
